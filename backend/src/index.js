530b73e74a35f2182ffe17cdd0dd1c34a3d1faab
Sachin-fsd <sachineducational555@gmail.com>
2026-02-07 17:09:35 +0530

    fix:bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,6 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI 
-// const MONGODB_URI =
-//   process.env.MONGODB_URI ||
-//   'mongodb+srv://sachineducational:sachin@cluster0.mybvg88.mongodb.net/hrms?retryWrites=true&w=majority';
-
 const client = new MongoClient(MONGODB_URI);
 let employeesCollection;
 let attendanceCollection;
